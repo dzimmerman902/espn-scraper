9a3b9380dd6640a94f5bddcc385e4f8f305aad52
Devin Zimmerman <dzimmerman902@gmail.com>
2021-06-12 20:39:37 -0600

    Some starter code and config

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// 1. Find base espn url to use
+// 2. Create an array of IDs to scan through
+// 3. Make request using base url and ids
+// 4. Retrieve html response from axios.
+// 5. Load into cheerio.
+// 6. Need to figure out what fields can be pulled from the html.
+// 7. Grab the require data from the fields.
+// 8. Need to transform values according to sport.
+// 9. Pass into write field to save into database.
+
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+(async () => {
+    const baseURL = 'https://www.espn.com/nba/player/_/id';
+    const playerEspnId = '4277905';
+
+    const body: string = await axios.get(`${baseURL}/${playerEspnId}`);
+
+    const $ = cheerio.load(body);
+})();
